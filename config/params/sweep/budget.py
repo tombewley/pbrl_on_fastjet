01911fbbc93125299257b6cb54c6f351b554f3fc
tombewley <tom.bewley@bristol.ac.uk>
2022-02-08 10:40:36 +0000

    Add entries to budget sweep

--- config/params/sweep/budget.py
+++ config/params/sweep/budget.py
@@ -1,9 +1,9 @@
 P = {
     "pbrl": {
-        "feedback_budget": [100,200,300],
+        "feedback_budget": [100,200,300,600,1200,5000],
         "observe_freq": 50, 
         "feedback_freq": 500, 
-        "log_freq": 500,   
+        "log_freq": 0,   
         "num_episodes_before_freeze": 50000, 
     }
 }
