814d6c02ba79c99e3b5932ef93c2f511bd18d4b0
tombewley <tom.bewley@bristol.ac.uk>
2022-02-08 10:38:07 +0000

    Don't log during schedule sweep

--- config/params/sweep/schedule.py
+++ config/params/sweep/schedule.py
@@ -3,7 +3,7 @@ P = {
         "feedback_budget": 10000,
         "observe_freq":               [10,    30,     70,     90   ],
         "feedback_freq":              [100,   300,    700,    900  ],
-        "log_freq":                   [100,   300,    700,    900  ],
+        "log_freq":                   [0,     0,      0,      0    ],
         "num_episodes_before_freeze": [10000, 30000,  70000,  90000]
     }
 }
