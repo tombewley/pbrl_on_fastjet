99c940c03c978c02c9c2f2e6c28b09f53186b480
tombewley <tom.bewley@bristol.ac.uk>
2022-02-14 16:23:53 +0000

    Add baseline params to sweep

---
+++ config/params/sweep/none.py
@@ -0,0 +1,10 @@
+P = {
+    "pbrl": {
+        "feedback_budget": 10000,
+        "scheduling_coef": 0,
+        "observe_freq": 50, 
+        "feedback_freq": 500, 
+        "log_freq": 0,   
+        "num_episodes_before_freeze": 50000, 
+    }
+}
