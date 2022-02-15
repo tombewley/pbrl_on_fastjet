4fc3a9c360fb085601eb0ac5615aa6d641aaf7d5
tombewley <tom.bewley@bristol.ac.uk>
2022-02-15 17:26:45 +0000

    Add harder target task

---
+++ config/params/tasks/target_hard.py
@@ -0,0 +1,8 @@
+SKIP_FRAMES = 25
+P = {"deployment": {
+    "task": "target_no_reward_hard",
+    "num_episodes": 100000,
+    "episode_time_limit": 750 / SKIP_FRAMES,
+    "skip_frames": SKIP_FRAMES
+    }
+}
