3f6e0e5f3fc6f3b5d43c5eb357a65d9286baf191
tombewley <tom.bewley@bristol.ac.uk>
2022-02-18 11:00:05 +0000

    Add noise_4 and noise_5

---
+++ config/params/tests/noise_4.py
@@ -0,0 +1,10 @@
+P = {
+    "pbrl": {
+        "interface": {
+            "gamma": 0.7,
+            "sigma": 40,
+            "epsilon": 0.3,
+            "p_equal": 0.2
+        },
+    }
+}
---
+++ config/params/tests/noise_5.py
@@ -0,0 +1,10 @@
+P = {
+    "pbrl": {
+        "interface": {
+            "gamma": 0.6,
+            "sigma": 50,
+            "epsilon": 0.4,
+            "p_equal": 0.2
+        },
+    }
+}
