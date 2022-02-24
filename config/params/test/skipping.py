20ebd824e56e854bd5e97b7d5924b25a902c9f71
tombewley <tom.bewley@bristol.ac.uk>
2022-02-24 14:30:58 +0000

    Rename manual -> human

--- config/params/test/skipping.py
+++ config/params/test/skipping.py
@@ -1,7 +1,15 @@
 P = {
+    "deployment": {
+        "num_episodes": 10
+    },
     "pbrl": {
+        "feedback_budget": 10,
+        "observe_freq": 1, 
+        "feedback_freq": 1, 
+        "num_episodes_before_freeze": 10, 
+        "scheduling_coef": 0,
         "interface": {
-            "d_skip": -50
+            "d_skip": -10,
         },
     }
 }
