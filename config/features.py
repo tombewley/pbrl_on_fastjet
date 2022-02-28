1fe2136d70433acb2e12a65060e45589bbd92b7a
tombewley <tom.bewley@bristol.ac.uk>
2022-02-28 13:57:02 +0000

    Move logging params out of test

--- config/features.py
+++ config/features.py
@@ -1,53 +1,63 @@
 """
  t   t+1
- 0 | 32  | fast_jet.pos.x
- 1 | 33  | fast_jet.pos.y
- 2 | 34  | fast_jet.pos.z
- 3 | 35  | fast_jet.vel.x
- 4 | 36  | fast_jet.vel.y
- 5 | 37  | fast_jet.vel.z
- 6 | 38  | fast_jet.acc.x
- 7 | 39  | fast_jet.acc.y
- 8 | 40  | fast_jet.acc.z
- 9 | 41  | fast_jet.r_vel.x
-10 | 42  | fast_jet.r_vel.y
-11 | 43  | fast_jet.r_vel.z
-12 | 44  | fast_jet.axis.x
-13 | 45  | fast_jet.axis.y
-14 | 46  | fast_jet.axis.z
-15 | 47  | fast_jet.up.x
-16 | 48  | fast_jet.up.y
-17 | 49  | fast_jet.up.z
-18 | 50  | fast_jet.thrust
-19 | 51  | target.pos.x
-20 | 52  | target.pos.y
-21 | 53  | target.pos.z
-22 | 54  | target.axis.x
-23 | 55  | target.axis.y
-24 | 56  | target.axis.z
-25 | 57  | target.up.x
-26 | 58  | target.up.y
-27 | 59  | target.up.z
-28 | --  | demanded_pitch
-29 | --  | demanded_roll
-30 | --  | demanded_yaw
-31 | --  | demanded_thrust
+ 0 | 41  | ego.pos.x
+ 1 | 42  | ego.pos.y
+ 2 | 43  | ego.pos.z
+ 3 | 44  | ego.vel.x
+ 4 | 45  | ego.vel.y
+ 5 | 46  | ego.vel.z
+ 6 | 47  | ego.acc.x
+ 7 | 48  | ego.acc.y
+ 8 | 49  | ego.acc.z
+ 9 | 50  | ego.r_vel.x
+10 | 51  | ego.r_vel.y
+11 | 52  | ego.r_vel.z
+12 | 53  | ego.axis.x
+13 | 54  | ego.axis.y
+14 | 55  | ego.axis.z
+15 | 56  | ego.up.x
+16 | 57  | ego.up.y
+17 | 58  | ego.up.z
+18 | 59  | ego.thrust
+19 | 60  | target.pos.x
+20 | 61  | target.pos.y
+21 | 62  | target.pos.z
+22 | 63  | target.vel.x
+23 | 64  | target.vel.y
+24 | 65  | target.vel.z
+25 | 66  | target.acc.x
+26 | 67  | target.acc.y
+27 | 68  | target.acc.z
+28 | 69  | target.r_vel.x,
+29 | 70  | target.r_vel.y,
+30 | 71  | target.r_vel.z,
+31 | 72  | target.axis.x, 
+32 | 73  | target.axis.y,
+33 | 74  | target.axis.z,
+34 | 75  | target.up.x,
+35 | 76  | target.up.y,
+36 | 77  | target.up.z
+37 | --  | demanded_pitch
+38 | --  | demanded_roll
+39 | --  | demanded_yaw
+40 | --  | demanded_thrust
 """
 import numpy as np
+G_VEC = np.array([0,9.81,0])
 
 # NOTE: Makes more sense to evaluate performance w.r.t. *previous* target
 F = {}
-F["dist"]              = lambda t: np.linalg.norm(t[:,32:35] - t[:,19:22], axis=1)
+F["dist"]              = lambda t: np.linalg.norm(t[:,41:44] - t[:,19:22], axis=1)
 F["delta_dist"]        = lambda t: F["dist"](t) - np.linalg.norm(t[:,0:3] - t[:,19:22], axis=1)
-F["alt"]               = lambda t: t[:,33]
+F["alt"]               = lambda t: t[:,42]
 F["target_alt"]        = lambda t: t[:,20]
 F["alt_error"]         = lambda t: np.abs(F["alt"](t) - F["target_alt"](t))
-F["delta_alt_error"]   = lambda t: F["alt_error"](t) - np.abs(t[:,1] - t[:,20])
-F["dist_xz"]           = lambda t: (((t[:,32] - t[:,19])**2) + ((t[:,34] - t[:,21])**2))**0.5
+F["delta_alt_error"]   = lambda t: F["alt_error"](t) - np.abs(t[:,1] - F["target_alt"](t))
+F["dist_xz"]           = lambda t: (((t[:,41] - t[:,19])**2) + ((t[:,43] - t[:,21])**2))**0.5
 F["delta_dist_xz"]     = lambda t: F["dist_xz"](t) - (((t[:,0] - t[:,19])**2) + ((t[:,2] - t[:,21])**2))**0.5
-F["pitch"]             = lambda t: np.arcsin(t[:,45])
+F["pitch"]             = lambda t: np.arcsin(t[:,54])
 F["abs_pitch"]         = lambda t: np.abs(F["pitch"](t))
-F["target_pitch"]      = lambda t: np.arcsin(t[:,23])
+F["target_pitch"]      = lambda t: np.arcsin(t[:,32])
 F["pitch_error"]       = lambda t: np.abs(F["pitch"](t) - F["target_pitch"](t))
 F["delta_pitch_error"] = lambda t: F["pitch_error"](t) - np.abs(np.arcsin(t[:,13]) - F["target_pitch"](t))
 # F["roll"]              = lambda t: np.arctan2(np.sum(___, axis=1), np.sum(___, axis=1)) # NOTE: BIT OF A MESS TO COMPUTE
@@ -55,19 +65,21 @@ F["delta_pitch_error"] = lambda t: F["pitch_error"](t) - np.abs(np.arcsin(t[:,13
 # F["target_roll"]       = lambda t: np.arctan2(np.sum(___, axis=1), np.sum(___, axis=1)) 
 # F["roll_error"]        = lambda t: np.abs(F["roll"](t) - F["target_roll"](t))
 # F["delta_roll_error"]  = lambda t: F["roll_error"](t) - np.abs(np.arctan2(np.sum(___, axis=1), np.sum(___, axis=1)) - F["target_roll"](t))
-# Due to symmetry, no reason for absolute heading and target heading to be meaningful
-F["hdg_error"]         = lambda t: np.abs(np.arctan2(t[:,46], t[:,44]) - np.arctan2(t[:,24], t[:,22]))
-F["delta_hdg_error"]   = lambda t: F["hdg_error"](t) - np.abs(np.arctan2(t[:,14], t[:,12]) - np.arctan2(t[:,24], t[:,22]))
-F["fwd_error"]         = lambda t: np.arccos(np.sum(t[:,44:47]*t[:,22:25], axis=1))
-F["delta_fwd_error"]   = lambda t: F["fwd_error"](t) - np.arccos(np.sum(t[:,12:15]*t[:,22:25], axis=1))
-F["up_error"]          = lambda t: np.arccos(np.sum(t[:,47:50]*t[:,25:28], axis=1))
-F["delta_up_error"]    = lambda t: F["up_error"](t) - np.arccos(np.sum(t[:,15:18]*t[:,25:28], axis=1))
-F["right_error"]       = lambda t: np.arccos(np.sum(np.cross(t[:,44:47], t[:,47:50])*np.cross(t[:,22:25], t[:,25:28]), axis=1))
-F["delta_right_error"] = lambda t: F["right_error"](t) - np.arccos(np.sum(np.cross(t[:,12:15], t[:,15:18])*np.cross(t[:,22:25], t[:,25:28]), axis=1))
-F["abs_vel"]           = lambda t: np.linalg.norm(t[:,35:38], axis=1)
-F["abs_acc"]           = lambda t: np.linalg.norm(t[:,38:41], axis=1)
-F["abs_rot_vel"]       = lambda t: np.linalg.norm(t[:,41:44], axis=1)
-F["thrust"]            = lambda t: t[:,50]
+# NOTE: Due to symmetry, no reason for absolute heading and target heading to be meaningful
+F["hdg_error"]         = lambda t: np.abs(np.arctan2(t[:,55], t[:,53]) - np.arctan2(t[:,33], t[:,31]))
+F["delta_hdg_error"]   = lambda t: F["hdg_error"](t) - np.abs(np.arctan2(t[:,14], t[:,12]) - np.arctan2(t[:,33], t[:,31]))
+F["fwd_error"]         = lambda t: np.arccos(np.sum(t[:,53:56]*t[:,31:34], axis=1))
+F["delta_fwd_error"]   = lambda t: F["fwd_error"](t) - np.arccos(np.sum(t[:,12:15]*t[:,31:34], axis=1))
+F["up_error"]          = lambda t: np.arccos(np.sum(t[:,56:59]*t[:,34:37], axis=1))
+F["delta_up_error"]    = lambda t: F["up_error"](t) - np.arccos(np.sum(t[:,15:18]*t[:,34:37], axis=1))
+F["right_error"]       = lambda t: np.arccos(np.sum(np.cross(t[:,53:56], t[:,56:59])*np.cross(t[:,31:34], t[:,34:37]), axis=1))
+F["delta_right_error"] = lambda t: F["right_error"](t) - np.arccos(np.sum(np.cross(t[:,12:15], t[:,15:18])*np.cross(t[:,31:34], t[:,34:37]), axis=1))
+F["abs_vel"]           = lambda t: np.linalg.norm(t[:,44:47], axis=1)
+F["g_force"]           = lambda t: np.linalg.norm(t[:,47:50]+G_VEC, axis=1) / 9.81 # NOTE: Includes gravity
+F["pitch_rate"]        = lambda t: np.abs(t[:,50])
+F["roll_rate"]         = lambda t: np.abs(t[:,51]) 
+F["yaw_rate"]          = lambda t: np.abs(t[:,52])
+F["thrust"]            = lambda t: t[:,59]
 F["delta_thrust"]      = lambda t: np.abs(F["thrust"](t) - t[:,18])
 
 # TODO: Action rewards?
---
+++ config/params/log.py
@@ -0,0 +1,9 @@
+P={
+    "deployment": {
+        "checkpoint_freq": 10000,
+    },
+    "pbrl": {
+        "log_freq": 500,      
+        "save_freq": 5000
+    }
+}
