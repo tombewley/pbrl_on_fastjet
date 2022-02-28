2d13fe83309b5344e28acdee9faf5907e700c335
tombewley <tom.bewley@bristol.ac.uk>
2022-02-28 17:05:09 +0000

    closing_speed instead of delta_dist

--- config/oracles.py
+++ config/oracles.py
@@ -8,15 +8,15 @@ def target_pose_linear(tr):
     return - (F["fwd_error"](tr) + F["up_error"](tr) + 0.01*F["dist"](tr)).sum()
 
 def target_pose_tree(tr):
-    def reward(d, d_d, f, u):
+    def reward(d, v, f, u):
         constant = -1.
         reward_d   = 0. if d < 20      else (-1. if d < 50      else -2.) 
-        reward_d_d = 0. if d_d < 0     else -1.
+        reward_v = 0. if v < 0     else -1.
         reward_f   = 0. if f < np.pi/4 else (-1. if f < np.pi/2 else -2.)
         reward_u   = 0. if u < np.pi/4 else (-1. if u < np.pi/2 else -2.)
-        return constant + reward_d + reward_d_d + reward_f + reward_u
-    return [reward(d, d_d, f, u) for d, d_d, f, u in zip(
-            F["dist"](tr), F["delta_dist"](tr), F["fwd_error"](tr), F["up_error"](tr))]
+        return constant + reward_d + reward_v + reward_f + reward_u
+    return [reward(d, v, f, u) for d, v, f, u in zip(
+            F["dist"](tr), F["closing_speed"](tr), F["fwd_error"](tr), F["up_error"](tr))]
     
 if False: # TODO: generic function to visualise 2D reward
     import numpy as np
