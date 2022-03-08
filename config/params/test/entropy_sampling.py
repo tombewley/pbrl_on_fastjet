21dd57a46f89b19a2117333ed7bc32a0c314cd3a
tombewley <tom.bewley@bristol.ac.uk>
2022-03-08 17:22:19 +0000

    Entropy sampling experiments

---
+++ config/params/test/entropy_sampling.py
@@ -0,0 +1,24 @@
+P = {
+    "deployment": {
+        "num_episodes": 40
+    },
+    "pbrl": {
+        "feedback_budget": 40, 
+        "observe_freq": 1, 
+        "feedback_freq": 2,
+        "num_episodes_before_freeze": 40, 
+        "scheduling_coef": 0,
+        "sampler": {
+            "weight": "entropy", 
+            "constrained": False, 
+            "probabilistic": True,
+            "preference_eqn": "thurstone"
+        },
+        "logger": {
+            "freq": 2,
+            "plots": {
+                "sampler_matrix"
+            }
+        }
+    }
+}
