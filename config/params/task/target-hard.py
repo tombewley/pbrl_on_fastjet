76536ccb3cd95e89b394926d5292122c86d2d242
tombewley <tom.bewley@bristol.ac.uk>
2022-03-08 11:45:28 +0000

    Add target-med

--- config/params/task/target-hard.py
+++ config/params/task/target-hard.py
@@ -2,7 +2,7 @@ SKIP_FRAMES = 25
 
 P = {
     "deployment": {
-        "task": "target_hard",
+        "task": "target-hard",
         "episode_time_limit": 750 / SKIP_FRAMES,
         "skip_frames": SKIP_FRAMES
     }
---
+++ config/params/task/target-med.py
@@ -0,0 +1,9 @@
+SKIP_FRAMES = 25
+
+P = {
+    "deployment": {
+        "task": "target-hard",
+        "episode_time_limit": 750 / SKIP_FRAMES,
+        "skip_frames": SKIP_FRAMES
+    }
+}
