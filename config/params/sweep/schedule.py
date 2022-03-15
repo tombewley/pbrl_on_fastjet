7654ed5b4f81b66b09decbf9ca564e5c77e00f41
tombewley <tom.bewley@bristol.ac.uk>
2022-03-15 13:37:53 +0000

    Update schedule sweep

--- config/params/sweep/schedule.py
+++ config/params/sweep/schedule.py
@@ -4,6 +4,12 @@ P = {
         "scheduling_coef": 0,
         "observe_freq":               [10,    30,     70,     90   ],
         "feedback_freq":              [100,   300,    700,    900  ],
-        "num_episodes_before_freeze": [10000, 30000,  70000,  90000]
+        "num_episodes_before_freeze": [10000, 30000,  70000,  90000],
+        "sampler": {
+            "weight": "ucb", 
+            "constrained": True, 
+            "probabilistic": True, 
+            "num_std": 0 # NOTE: <<<<
+        }
     }
 }
