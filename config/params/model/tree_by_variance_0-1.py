5cf0b665c6a80101866d1d4c76ebeb0b28a6926b
tombewley <tom.bewley@bristol.ac.uk>
2022-03-22 11:47:59 +0000

    Add 0-1 loss tree

---
+++ config/params/model/tree_by_variance_0-1.py
@@ -0,0 +1,19 @@
+from rlutils.observers.pbrl.models import RewardTree
+
+P = {
+    "pbrl": {
+        "model": {
+            "kind": RewardTree,
+            "preference_eqn": "thurstone",
+            "loss_func": "0-1",
+            "split_by_variance": True,
+            "p_clip": 0.1,
+            "m_max": 100,
+            "num_from_queue": float("inf"),
+            "min_samples_leaf": 1,
+            "store_all_qual": False,
+            "alpha": 0.001,
+        },
+        "reward_source": "model"
+    }
+}
