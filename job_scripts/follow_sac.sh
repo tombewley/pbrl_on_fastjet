c8cced5492ab36b1962432a02e66600e9b8dcffd
tombewley <tom.bewley@bristol.ac.uk>
2022-04-12 17:53:09 +0100

    Use source activate instead of conda activate

--- job_scripts/follow_sac.sh
+++ job_scripts/follow_sac.sh
@@ -14,7 +14,6 @@ PARAMS=(
 )
 
 module load CUDA
-conda init bash
-conda activate env_pytorch
+source activate env_pytorch
 
 srun python run.py task.follow oracle=dist_closing_uperr wandb ${PARAMS[$SLURM_ARRAY_TASK_ID]}
