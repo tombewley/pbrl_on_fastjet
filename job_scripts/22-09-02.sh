42d008de3d0e049868f7fc79bc342c395c3f7d61
tombewley <tom.bewley@bristol.ac.uk>
2022-09-02 16:21:33 +0100

    Add 22-09-02 job script

--- job_scripts/22-09-02.sh
+++ job_scripts/22-09-02.sh
@@ -5,11 +5,53 @@
 #SBATCH --gres=gpu:2
 #SBATCH --partition gpu
 #SBATCH --time=2:00:00
-#SBATCH --array=0-1
+#SBATCH --array=0-43
 
 PARAMS=(
-    "env.fastjet.follow agent.pets=3 oracle.fastjet.follow.dist_closing_uperr_v2 features.fastjet.default sampler.ucb_nostd_norecency schedule.200_one_per_ep wandb model.tree.fullgraph=4"
-    "env.fastjet.follow agent.pets=3 oracle.fastjet.follow.dist_closing_uperr_v2 features.fastjet.default sampler.ucb_nostd_norecency schedule.200_one_per_ep wandb model.tree.fullgraph=5"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=net"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=0"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=4"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=8"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=12"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=net"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=0"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=4"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=8"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=12"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=net"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=0"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=4"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=8"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=12"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=net"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=0"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=4"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=8"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=ucb_nostd_norecency --schedule=1k_200_1_1 --model=tree=12"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=net"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=0"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=4"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=8"
+    "run.py fastjet.match pets=3 200 --wandb=1 --group=22-09-02_match --oracle=dist_pose_when_close --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=12"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=net"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=0"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=4"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=8"
+    "run.py fastjet.follow pets=3 200 --wandb=1 --group=22-09-02_follow --oracle=dist_closing_uperr_v2 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=12"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=net"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=0"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=4"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=8"
+    "run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-02_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=12"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=net"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=0"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=4"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=8"
+    "run.py fastjet.land pets=3 200 --wandb=1 --group=22-09-02_land --oracle=closing_and_shaping --features=default --sampler=uniform_recency --schedule=1k_200_1_1_sched --model=tree=12"
 )
 
 module load CUDA
