3b87d7534721af508d1fc567324e77e1a784e098
tombewley <tom.bewley@bristol.ac.uk>
2022-09-09 16:18:38 +0100

    Final tweak to new job script

--- job_scripts/22-09-09.sh
+++ job_scripts/22-09-09.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -login
 
-#SBATCH --job-name=pbrl_220902_extra
+#SBATCH --job-name=pbrl_220909
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:2
 #SBATCH --partition gpu
 #SBATCH --time=4:00:00
-#SBATCH --array=0-15
+#SBATCH --array=0-23
 
 PARAMS=(
     "fastjet.match pets=3 200 --wandb=1 --group=22-09-09_match --save_freq=50 --oracle=dist_pose_when_close --features=default --sampler=uniform_recency --schedule=1k_200_1_1 --model=net"
