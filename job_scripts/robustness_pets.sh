e6667a1a13c9dc28a6394c5078d31a7b1e0c5620
tombewley <tom.bewley@bristol.ac.uk>
2022-09-13 13:40:11 +0100

    Update PETS job script to include land

--- job_scripts/robustness_pets.sh
+++ job_scripts/robustness_pets.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:2
 #SBATCH --partition gpu
-#SBATCH --time=10:00:00
-#SBATCH --array=0-8
+#SBATCH --time=5:00:00
+#SBATCH --array=0-11
 
 PARAMS=(
     "fastjet.match pets=2 200 --wandb=1 --group=robustness_pets_match --oracle=dist_pose_when_close --features=default --sampler=uniform_recency --schedule=1k_200_1_1 --model=tree=10"
@@ -17,6 +17,9 @@ PARAMS=(
     "fastjet.chase pets=2 200 --wandb=1 --group=robustness_pets_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=uniform_recency --schedule=1k_200_1_1 --model=tree=10"
     "fastjet.chase pets=4 200 --wandb=1 --group=robustness_pets_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=uniform_recency --schedule=1k_200_1_1 --model=tree=10"
     "fastjet.chase pets=5 200 --wandb=1 --group=robustness_pets_chase --oracle=dist20_los_roll_alt50 --features=default --sampler=uniform_recency --schedule=1k_200_1_1 --model=tree=10"
+    "fastjet.land pets=2 200 --wandb=1 --group=robustness_pets_land --oracle=closing_and_shaping --features=default --sampler=uniform_recency --schedule=1k_200_1_1 --model=tree=10"
+    "fastjet.land pets=4 200 --wandb=1 --group=robustness_pets_land --oracle=closing_and_shaping --features=default --sampler=uniform_recency --schedule=1k_200_1_1 --model=tree=10"
+    "fastjet.land pets=5 200 --wandb=1 --group=robustness_pets_land --oracle=closing_and_shaping --features=default --sampler=uniform_recency --schedule=1k_200_1_1 --model=tree=10"
 )
 
 module load CUDA
