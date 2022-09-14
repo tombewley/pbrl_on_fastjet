1ebc5e5fb74395d842a35a902d27de05086339f1
tombewley <tom.bewley@bristol.ac.uk>
2022-09-14 17:32:10 +0100

    Add fallback CPU job script

---
+++ job_scripts/chase_net_on_cpu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -login
+
+#SBATCH --job-name=pbrl_land200
+#SBATCH --nodes=1
+#SBATCH --partition cpu
+#SBATCH --time=4:00:00
+
+source activate env_pytorch
+
+srun python run.py fastjet.chase pets=3 200 --wandb=1 --group=22-09-09_chase --save_freq=50 --oracle=dist20_los_roll_alt50 --features=default --sampler=uniform_recency --schedule=1k_200_1_1 --model=net
