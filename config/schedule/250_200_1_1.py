57880b0080adea30b182deb94c12c2858487a08a
tombewley <tom.bewley@bristol.ac.uk>
2022-09-14 17:52:11 +0100

    Add budget study

---
+++ config/schedule/250_200_1_1.py
@@ -0,0 +1,9 @@
+P = {
+    "pbrl": {
+        "feedback_budget": 250, 
+        "observe_freq": 1,
+        "feedback_freq": 1,
+        "feedback_period": 200,
+        "scheduling_coef": 0
+    }
+}
