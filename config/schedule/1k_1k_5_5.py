478cbfbd1f0a6f402a7ca59b49166a196db70f20
tombewley <tom.bewley@bristol.ac.uk>
2022-09-18 17:36:35 +0100

    1k SAC runs

---
+++ config/schedule/1k_1k_5_5.py
@@ -0,0 +1,9 @@
+P = {
+    "pbrl": {
+        "feedback_budget": 1000, 
+        "observe_freq": 5, 
+        "feedback_freq": 5,
+        "feedback_period": 1000, 
+        "scheduling_coef": 0
+    }
+}
